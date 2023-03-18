c743a59767edd39f845fdd191263de2a344a54c5
SDSirius <114844732+SDSirius@users.noreply.github.com>
2023-03-18 11:42:33 -0300

    Aula 2 Auth login/Pass

---
+++ src/auth/helpers/messages.helpers.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper ={
+    AUTH_LOGIN_NOT_FOUND: 'Favor preencher o login.',
+    AUTH_PASSWORD_NOT_FOUND: 'Favor preencher a senha.',
+    AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: 'E-mail e/ou Senha não encontrados.',
+}
